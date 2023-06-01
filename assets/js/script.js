ffe0a3d0694608b72466450abdde1e51d3fe0eae
Jake <jakeziah@gmail.com>
2023-06-01 12:19:13 -0700

    initial file commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var h1 = document.getElementById('h1');
+
+h1.textContent = "Coding Quiz";
+h1.style.fontSize = '75px';
+
+var words = document.getElementById('words');
+words.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds.";
+words.style.fontSize = '30px';
+words.style.margin = '5px';
+
+var startButton = document.getElementById('btn');
+btn.textContent = "Start";
+/*
+var hideCount = document.getElementById("count");
+hideCount.style.visibility = "hidden";
+if (hideCount === 'hidden') {
+    var hiddenButtons = true;
+} else {
+    document.getElementById('count').style.visibility = 'visible'
+}
+*/
+
