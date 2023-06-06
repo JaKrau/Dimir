befea318a935ab3aa635167eac267abf9e7c1364
JaKrau <jakeziah@gmail.com>
2023-06-06 10:41:12 -0700

    add semantic html and reformat script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,25 @@
+var scoreHolder = document.getElementById('scoreHolder');
 var timerHolder = document.getElementById('timerHolder');
+var h1 = document.getElementById('h1');
+var words = document.getElementById('words');
+var startButton = document.getElementById('btn');
+
+scoreHolder.style.fontSize = 'xx-large';
 timerHolder.style.fontSize = 'xx-large';
-// Our timer has a text size
+h1.style.fontSize = '75px';
+words.style.fontSize = 'xx-large';
+words.style.margin = '5px';
+
+h1.textContent = "Coding Quiz";
+words.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds.";
+btn.textContent = "Start";
+
 
 function renderTimerToBrowser() {
     timerHolder.textContent = "Time left: ";
 }
 
 
-var h1 = document.getElementById('h1');
-h1.textContent = "Coding Quiz";
-h1.style.fontSize = '75px';
-
-
-var words = document.getElementById('words');
-words.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds.";
-words.style.fontSize = 'xx-large';
-words.style.margin = '5px';
-
-
-var startButton = document.getElementById('btn');
-btn.textContent = "Start";
-
-
-/**/
 
 
 renderTimerToBrowser();
