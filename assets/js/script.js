56476f0492f0bbb5849bb3f9676960dea8f2809b
JaKrau <jakeziah@gmail.com>
2023-06-10 09:27:05 -0700

    add some start button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Connecting all html elements we need to JS so we can touch 'em
 var scoreHolder = document.getElementById('scoreHolder');
 var timerHolder = document.getElementById('timerHolder');
 var h1 = document.getElementById('h1');
@@ -5,27 +6,33 @@ var words = document.getElementById('words');
 var startButton = document.getElementById('btn');
 var questionSection = document.getElementById('questions');
 
+// Countdown to Extinction! (good album)
 var timer = 90;
 
+// Style settings! (not elegant but I can see it without getting a headache)
 scoreHolder.style.fontSize = 'xx-large';
 timerHolder.style.fontSize = 'xx-large';
 h1.style.fontSize = '75px';
 words.style.fontSize = 'xx-large';
 words.style.margin = '5px';
-//questionSection.style.visibility = 'hidden';
+questionSection.style.visibility = 'hidden';
 
+// Content! (text and such in variables we can re-use)
 h1.textContent = "Coding Quiz";
 words.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds.";
 btn.textContent = "Start";
 
-
+// Behold, the button! Birthing our countdown.
 function renderTimerToBrowser() {
     timerHolder.textContent = "Time left: " + timer;
 }
 
 startButton.addEventListener("click", function() {
     var interval = setInterval(function() {
-        
+        startButton.style.visibility = 'hidden';
+        questionSection.style.visibility = 'visible';
+        words.textContent = "";
+        h1.textContent = "Test Question 1";
         timer --;
          
     if (timer <= 0) {
@@ -36,9 +43,3 @@ startButton.addEventListener("click", function() {
 
     }, 1000);
 });
-
-
-
-
-
-renderTimerToBrowser();
