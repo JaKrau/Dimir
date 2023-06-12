c2248cb24cd6457c32cacd02e8ce9f4c23061d46
JaKrau <jakeziah@gmail.com>
2023-06-11 21:47:14 -0700

    started adding divs for each question, but about to go in a different direction

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,16 @@ var timerHolder = document.getElementById('timerHolder');
 var h1 = document.getElementById('h1');
 var words = document.getElementById('words');
 var startButton = document.getElementById('btn');
-var questionSection = document.getElementById('questions');
+var questionSection = document.getElementById('questionSection');
+var firstQuestion = document.getElementById('question1');
+var qButton1 = questionSection.children[0];
+var qButton2 = questionSection.children[1];
+var qButton3 = questionSection.children[2];
+var qButton4 = questionSection.children[3];
 
 // Countdown to Extinction! (good album)
 var timer = 90;
+var score = 0;
 
 // Style settings! (not elegant but I can see it without getting a headache)
 scoreHolder.style.fontSize = 'xx-large';
@@ -15,9 +21,9 @@ timerHolder.style.fontSize = 'xx-large';
 h1.style.fontSize = '75px';
 words.style.fontSize = 'xx-large';
 words.style.margin = '5px';
-questionSection.style.visibility = 'hidden';
+questionSection.style.display = 'none';
 
-// Content! (text and such in variables we can re-use)
+// Content! 
 h1.textContent = "Coding Quiz";
 words.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds.";
 btn.textContent = "Start";
@@ -29,10 +35,10 @@ function renderTimerToBrowser() {
 
 startButton.addEventListener("click", function() {
     var interval = setInterval(function() {
-        startButton.style.visibility = 'hidden';
-        questionSection.style.visibility = 'visible';
+        startButton.style.display = 'none';
+        firstQuestion.style.display = 'block';
         words.textContent = "";
-        h1.textContent = "Test Question 1";
+
         timer --;
          
     if (timer <= 0) {
@@ -40,6 +46,74 @@ startButton.addEventListener("click", function() {
     }
 
     renderTimerToBrowser();
+    questionGeneration1();
 
     }, 1000);
 });
+
+// Parent 'questions' div needs children with purpose! This seems like a mess of code, but I can't think of how to clean it up efficiently. If anyone reads this and has suggestions, please tell.
+function questionGeneration1() {
+    h1.textContent = "Which HTML element is used for in-line JavaScript?";
+    qButton1.textContent = "<link>";
+    qButton2.textContent = "<js>";
+    qButton3.textContent = "<script>";
+    qButton4.textContent = "<coding>";
+
+    qButton1.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton1.textContent = '';
+        questionGeneration2();
+    });
+    qButton2.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton2.textContent = '';
+        questionGeneration2();
+    });
+    qButton3.addEventListener("click", function() {
+        score = score + 1;
+        console.log(score);
+        questionGeneration2();
+    });
+    qButton4.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton4.textContent = '';
+        questionGeneration2();
+    });
+
+
+} 
+
+function questionGeneration2() {
+    h1.textContent = "How do you write comments in JavaScript?";
+    qButton1.textContent = "\\";
+    qButton2.textContent = "<!-- -->";
+    qButton3.textContent = "#";
+    qButton4.textContent = "//";
+
+    qButton1.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton1.textContent = '';
+    });
+    qButton2.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton2.textContent = '';
+    });
+    qButton3.addEventListener("click", function() {
+        timer = timer -10;
+        h1.textContent = '';
+        qButton4.textContent = '';
+    });
+    qButton4.addEventListener("click", function() {
+        score = score + 1;
+        console.log(score);
+    });
+    
+}
+
+renderTimerToBrowser();
+
